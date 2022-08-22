52e1d3b1a17dcceb059259b64e95a22e67d08bb2
Ádám Pásztor <mrpasztoradam@gmail.com>
2022-08-22 18:07:49 +0200

    mobil, teblet, web responzivitas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <nav>navbar</nav>
+        <div id="sidebar">Sidebar</div>
+        <div id="content1">
+          Content 1
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+            nihil molestias quo ducimus quaerat maiores delectus, repellat
+            perspiciatis sapiente, distinctio nesciunt porro voluptates quos
+            doloremque tempora deserunt impedit obcaecati eligendi quisquam!
+            Animi voluptates laudantium modi? Consectetur sed odio ut sit?
+          </p>
+        </div>
+        <div id="content2">
+          Content 2
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam nam
+            illum cumque perferendis consequatur, quam nisi amet numquam dolorem
+            nihil asperiores voluptatum similique sapiente, expedita pariatur
+            veritatis ab iure a ut ea modi animi delectus provident rerum?
+            Corporis sit velit nam temporibus consectetur sed rem, ex
+            perspiciatis! Modi nemo veniam molestias veritatis ad facilis fugiat
+            vero fuga, suscipit voluptatibus?
+          </p>
+        </div>
+        <div id="content3">Content 3</div>
+        <div id="bigcontent">Bigcontent</div>
+      </div>
     </div>
   );
 }
